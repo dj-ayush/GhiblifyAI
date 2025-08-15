cff8d39fc338bf34153716130632d10dffa6d007
dj-ayush <ayushgupta.7ag@gmail.com>
2025-08-15 15:00:03 +0530

    initializing frontend proj setup with configs

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#006d77',
+          light: '#83c5be',
+        },
+        accent: '#ffddd2',
+        background: '#fffdf5',
+      },
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+        funky: ['"Fredoka One"', 'cursive'],
+      },
+    },
+  },
+}
